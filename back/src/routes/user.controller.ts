6b9036cbce42dd4814cf660a67c9b2ca9c5738e5
seokho10007 <sukho1007@naver.com>
2021-12-16 23:34:56 +0900

    Feat. Password Change route 생성

--- back/src/routes/user.controller.ts
+++ back/src/routes/user.controller.ts
@@ -57,4 +57,32 @@ router.post("/password/reset", async (req, res, next) => {
   }
 });
 
+router.post("/password/change", async (req, res, next) => {
+  passport.authenticate("jwt", { session: false }, async (err, _user: ITokenUser) => {
+    if (err) return next(err);
+    if (!_user) return res.status(401).json({ status: false, message: "로그인 후 가능합니다." });
+
+    // 임시 비밀번호, 비밀번호
+    const { currentPassword, password } = req.body;
+
+    const user = (await userService.getById(_user.id, { refreshToken: 0 })) as IUserDocument;
+
+    const isCompared = await user.comparePassword(currentPassword);
+
+    if (!isCompared) return res.json({ status: false, message: "비밀번호가 일치하지 않습니다." });
+
+    const hashedPassword = await makeHashPassword(password);
+
+    await userService.updateByQuery(
+      { _id: user.id },
+      { password: hashedPassword, passwordReset: false },
+    );
+
+    await userService.updateByQuery({ _id: _user.id }, { refreshToken: null });
+    res.clearCookie(jwtContents.header);
+
+    res.json({ status: true, message: "비밀번호 변경이 완료되었습니다. 다시 로그인 해주세요" });
+  })(req, res, next);
+});
+
 export default router;
