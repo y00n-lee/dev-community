21884e64d340744831fe06cfb0769b80593853c7
y00n-lee <0613myday@gmail.com>
2021-12-17 17:37:51 +0900

    feat. 모집게시판 카드리스트 제작

--- front/src/js/gather.js
+++ front/src/js/gather.js
@@ -11,12 +11,14 @@
 console.log("hey");
 function getLists() {
   let list = document.getElementById("gather-list");
-  list.innerHTML += `<article class="gather-list-items">
-                      <h3>제목</h3>
-                      <h4>제목</h4>
-                      <p class="p-text">
-                        내용내용
-                      </p>
-                    </article>`;
+  for (let i = 0; i < 9; i++) {
+    list.innerHTML += `<article class="gather-list-items">
+    <h3>제목</h3>
+    <h4>기술스택</h4>
+    <p class="p-text">
+      내용내용
+    </p>
+  </article>`;
+  }
 }
 getLists();
