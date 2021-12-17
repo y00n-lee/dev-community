13edd3e38d7231069dfa507940a68e1b9cb98eda
y00n-lee <0613myday@gmail.com>
2021-12-17 14:31:25 +0900

    feat. 게시판 목록 레이아웃 제작

---
+++ front/src/js/gather.js
@@ -0,0 +1,22 @@
+/* <div class="flex-item">
+  <img class="image" src="./img/about/man1.png" alt="" srcset="" />
+  <h3>장병연</h3>
+  <h4>프론트엔드/팀장</h4>
+  <p class="p-text">
+    안녕하세요, 장병연입니다. 안녕하세요, 장병연입니다. 안녕하세요, 장병연입니다. 안녕하세요,
+    장병연입니다. 안녕하세요, 장병연입니다. 안녕하세요, 장병연입니다. 안녕하세요, 장병연입니다.
+    안녕하세요, 장병연입니다. 안녕하세요, 장병연입니다.
+  </p>
+</div>; */
+console.log("hey");
+function getLists() {
+  let list = document.getElementById("gather-list");
+  list.innerHTML += `<article class="gather-list-items">
+                      <h3>제목</h3>
+                      <h4>제목</h4>
+                      <p class="p-text">
+                        내용내용
+                      </p>
+                    </article>`;
+}
+getLists();
