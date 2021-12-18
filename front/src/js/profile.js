bfcc77b5662142888b6e71c9b69dedea5fc4df86
Yoonseong Kim <rladbstjd96@naver.com>
2021-12-18 14:45:08 +0900

    중간저장

---
+++ front/src/js/profile.js
@@ -0,0 +1,5 @@
+const name = document.getElementById("nameVal");
+const email = document.getElementById("emailVal");
+const gender = document.getElementById("genderVal");
+const stacks = document.getElementById("stackList");
+const gitAddress = document.getElementById("gitVal");
---
+++ front/src/js/profileModify.js
@@ -0,0 +1,6 @@
+const modifyForm = document.getElementById("modifyForm");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const gender = document.getElementsByName("gender"); // 성별 radio
+const stacks = document.getElementsByName("stack"); // 기술 check
+const gitAddress = document.getElementById("gitAddress");
