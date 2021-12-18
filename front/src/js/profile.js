cbe4cef987eac036b7bcdaa7f7b84bc36b5a4243
Yoonseong Kim <rladbstjd96@naver.com>
2021-12-18 17:32:15 +0900

    불필요한 파일삭제/ 구조변경

--- front/src/js/profile.js
+++
@@ -1,5 +0,0 @@
-const name = document.getElementById("nameVal");
-const email = document.getElementById("emailVal");
-const gender = document.getElementById("genderVal");
-const stacks = document.getElementById("stackList");
-const gitAddress = document.getElementById("gitVal");
