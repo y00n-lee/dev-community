532c84713950fe9a3c3c302a54785ca5851b1090
seokho10007 <sukho1007@naver.com>
2021-12-20 00:25:24 +0900

    충돌 해결

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+
 import authRouter from "./auth";
 import usersRouter from "./user";
+import postsRouter from "./posts.controller";
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/user", usersRouter);
+router.use("/posts", postsRouter);
 
 export default router;
