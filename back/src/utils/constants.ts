3929f8be180bff9e9e03e9cc4df729614e7053ad
seokho10007 <sukho1007@naver.com>
2021-12-19 23:51:02 +0900

    Chore. Refresh Token secret content 추가

--- back/src/utils/constants.ts
+++ back/src/utils/constants.ts
@@ -5,8 +5,11 @@ config();
 export const jwtContents = {
   secret: process.env.JWT_SECRET_KEY as string,
   header: process.env.JWT_HEADER as string,
+  header_refresh: process.env.JWT_HEADER_REFRESH as string,
 };
 
 export const secretContents = {
   signin: process.env.SIGNIN_SECRET_KEY as string,
 };
+
+export const url = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000/";
