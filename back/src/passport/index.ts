b0ca87c1bec28f51bcd4464b89453a8f2a3e52aa
seokho10007 <sukho1007@naver.com>
2021-12-19 23:48:45 +0900

    Feat. Refresh Token 만료여부 검사 Strategy 생성 #22

--- back/src/passport/index.ts
+++ back/src/passport/index.ts
@@ -1,9 +1,10 @@
 import passport from "passport";
 
 import { LocalStrategy } from "./strategies/local.strategy";
-import { JwtStrategy } from "./strategies/jwt.strategy";
+import { JwtStrategy, ReFreshJwtStrategy } from "./strategies/jwt.strategy";
 
 export const PassportConfigunation = () => {
   passport.use("local", LocalStrategy);
   passport.use("jwt", JwtStrategy);
+  passport.use("refresh-jwt", ReFreshJwtStrategy);
 };
--- back/src/passport/strategies/jwt.strategy.ts
+++ back/src/passport/strategies/jwt.strategy.ts
@@ -1,24 +1,33 @@
 import { Strategy as JStrategy, ExtractJwt, VerifiedCallback } from "passport-jwt";
-import { userService } from "@src/services/user.service";
 import { jwtContents } from "@src/utils/constants";
-import { IUserDocument } from "@src/types/User";
 
 const JwtOpt = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  ignoreExpiration: false,
   secretOrKey: jwtContents.secret,
 };
 
-const JwtVerify = async (payload: any, done: VerifiedCallback) => {
+const JwtVerify = (payload: any, done: VerifiedCallback) => {
   try {
-    if (!payload) return done(null, false);
+    if (!payload) return done(null, false, { message: "로그인 후 이용 가능합니다." });
 
-    const { id } = (await userService.getById(payload.id, { password: 0 })) as IUserDocument;
+    done(null, { id: payload.id });
+  } catch (e: any) {
+    done(e, false);
+  }
+};
 
-    return done(null, { id });
+const RefreshJwtOpt = {
+  jwtFromRequest: ExtractJwt.fromHeader("refresh"),
+  secretOrKey: jwtContents.secret,
+};
+
+const RefreshJwtVerify = async (payload: any, done: VerifiedCallback) => {
+  try {
+    done(null, { id: payload.id });
   } catch (e: any) {
     done(e, false);
   }
 };
 
 export const JwtStrategy = new JStrategy(JwtOpt, JwtVerify);
+export const ReFreshJwtStrategy = new JStrategy(RefreshJwtOpt, RefreshJwtVerify);
