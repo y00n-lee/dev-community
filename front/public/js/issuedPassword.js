d185027e8708e1b996fccbb45127244827ee930a
Yoonseong Kim <rladbstjd96@naver.com>
2021-12-21 02:17:25 +0900

    feat 비밀번호찾기 dummy 데이터를 이용한 확인, 버튼 hover 시 cursor변경

--- front/public/js/issuedPassword.js
+++ front/public/js/issuedPassword.js
@@ -1,5 +1,6 @@
 import { makeHeader } from "./components/header.js";
 import { makeFooter } from "./components/footer.js";
+import { resetPassword } from "./api/dummy/index.js";
 
 //Body
 const container = document.querySelector(".container");
@@ -16,26 +17,20 @@ container.appendChild(searchSection);
 // 아이디 입력 여부
 // - 아이디가 있다면 임시 비밀번호가 발송되었다는 메시지와 로그인창 이동
 // - 아이디가 없다면 가입된 정보가 없습니다.
-const searchBtn = document.querySelector(".submit");
+const searchForm = document.querySelector("#searchForm");
+const formEmail = document.querySelector(".id");
 
-searchBtn.addEventListener("click", () => {
-  alert("임시 비밀번호가 발송되었습니다!");
-  window.location = "/signin";
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = formEmail.value;
+
+  if (!email) alert("아이디를 입력해주세요!");
+  else
+    resetPassword(email).then((res) => {
+      alert(res.message);
+      if (res.status) window.location = "/";
+    });
 });
-
-// const searchForm = document.querySelector("#searchForm");
-// const formEmail = document.getElementsByName("id")[0];
-
-// searchForm.addEventListener("submit", async (e) => {
-//   e.preventDefault();
-
-//   if (!formEmail.value) {
-//     alert("아이디를 입력해주세요!");
-//   } else {
-//     searchForm.submit();
-//     window.location = "/signin";
-//   }
-// });
 
 // Footer
 const footer = makeFooter();
