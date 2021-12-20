348144bfc46e74b9d5a9ac5778e3fb3095033de6
y00n-lee <0613myday@gmail.com>
2021-12-20 15:59:19 +0900

    header 수정 about 메뉴 삭제, 유틸 함수 적용

--- front/public/js/components/header.js
+++ front/public/js/components/header.js
@@ -1,26 +1,12 @@
-// Create Element Id
-function createEleId(el, id) {
-  const ele = document.createElement(`${el}`);
-  ele.setAttribute("id", `${id}`);
-
-  return ele;
-}
-
-// Create Elemen Class
-function addEleClass(el, className) {
-  const ele = document.createElement(`${el}`);
-  ele.classList.add(`${className}`);
-
-  return ele;
-}
+import { createEleId, createEleClass } from "./utils.js";
 
 export function makeHeader() {
   const header = createEleId("header", "header");
-  const nav = addEleClass("nav", "navBar");
-  const slider = addEleClass("div", "slider");
-  const navMenu = addEleClass("ul", "navMenu");
-  const navLogin = addEleClass("ul", "navLogin");
-  const menu = addEleClass("div", "menu");
+  const nav = createEleClass("nav", "navBar");
+  const slider = createEleClass("div", "slider");
+  const navMenu = createEleClass("ul", "navMenu");
+  const navLogin = createEleClass("ul", "navLogin");
+  const menu = createEleClass("div", "menu");
 
   slider.innerHTML = `<input class="burger-check" type="checkbox" id="burger-check" />
 <label class="burger-icon" for="burger-check">
@@ -29,14 +15,12 @@ export function makeHeader() {
 
   navMenu.innerHTML = `
     <li><a href="/">Logo</a></li>
-    <li><a href="#">About</a></li>
     <li><a href="/posts">모집게시판</a></li>`;
 
   navLogin.innerHTML = `<li class="login"><a href="/signin">Log In</a></li>
 <li class="register"><a href="signup">Register</a></li>`;
 
   menu.innerHTML = `<div style="width: 200px;">
-<a href="#">About</a>
 <a href="/posts">모집게시판</a>
 </div>`;
 
