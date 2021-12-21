612fe568e784dbad2bcfb07ae9127977220b7f76
y00n-lee <0613myday@gmail.com>
2021-12-22 03:55:55 +0900

    feat. 유저 프로필 업데이트 api 연결

--- front/public/js/userUpdate.js
+++ front/public/js/userUpdate.js
@@ -1,22 +1,19 @@
 import { makeHeader } from "./components/header.js";
 import { makeFooter } from "./components/footer.js";
-import { getUserInfo } from "./api/dummy/index.js";
+import { editUserInfo, getUserInfo } from "./api/dummy/index.js";
 import { makeSkillTag } from "./components/tag.js";
+import { removeChildsAll } from "./components/utils.js";
+import { changePassword } from "./api/user/changePassword.js";
+
 //DOM elements
 const body = document.querySelector("body");
 const header = makeHeader();
 const footer = makeFooter();
-
-const main = document.querySelector("#main");
-const title = document.querySelector("#main .group-title");
-
-const nameForm = document.getElementById("nickname");
-const tagForm = document.getElementById("skilltag");
-
-const nowPw = document.getElementById("nowPw");
-const changePw = document.getElementById("changePw");
-const checkPw = document.getElementById("checkPw");
-const passwordBtn = document.getElementById("passwordBtn");
+const main = document.getElementById("main");
+const nameForm = document.getElementById("nicknameForm");
+const tagForm = document.getElementById("tagForm");
+const passwordForm = document.getElementById("passwordForm");
+const p = `<p class="label">기술스택</p>`;
 
 // Header, footer append
 body.insertBefore(header, main);
@@ -25,160 +22,99 @@ body.insertBefore(footer, document.querySelector("script"));
 const pathname = window.location.pathname.split("/");
 const currentUserId = pathname[pathname.length - 1];
 
-// get user data
+// page onload
 window.onload = setUpdateData();
 
+// display user nickname, tags
 function setUpdateData() {
   getUserInfo(currentUserId)
     .then((res) => {
       if (!res.status) return alert(res.message);
 
-      const { user } = res.data;
-      const name = document.querySelector("input#name");
-      name.placeholder = user.nickname;
-      // addTextNode(document.querySelector(".group-title"), `${user.nickname}님의 프로필`);
+      const { nickname, tags } = res.data.user;
+      const name = document.getElementById("nicknameValue");
+      console.log(nickname);
+      name.value = nickname;
 
-      // const skillTag = document.querySelector("input#skilltag");
-      // `<input type="checkbox" checked disabled style="display:none"/>${userData[i]}`
+      removeChildsAll(tagForm);
+      tagForm.innerHTML = p;
+      for (let i = 0; i < tags.length; i++) tagForm.appendChild(makeSkillTag(tags[i]));
     })
     .catch((e) => alert(e.message));
 }
 
-function makeUserInfo(user) {
-  addTextNode(document.querySelector(".group-title"), `${user.nickname}님의 프로필`);
+nameForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  btnSubmit("nickname");
+});
 
-  const field = [`nickname`, `email`, `gender`];
-  const fieldname = [`닉네임`, `이메일`, `성별`];
+tagForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  btnSubmit("tags");
+});
+passwordForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  confirmPassword();
+});
 
-  const fieldNum = field.length;
-  for (let i = 0; i < fieldNum; i++) {
-    const div = makeDataField(user[field[i]], fieldname[i]);
-    form.insertBefore(div, updateBtn);
+// nickname, tags 변경 이벤트 함수
+function btnSubmit(queryname) {
+  if (queryname === "nickname") {
+    const nickname = document.getElementById("nicknameValue").value;
+    editUserInfo(currentUserId, nickname, `${queryname}`)
+      .then((res) => {
+        if (res.status) {
+          alert(res.message);
+          setUpdateData();
+        }
+      })
+      .catch((e) => alert(e.message));
+  } else {
+    const tags = document.querySelectorAll(`.tag`);
+    const tagList = [];
+    tags.forEach((ele) => {
+      if (ele.firstChild.cheked) tagList.push(ele.innerText);
+    });
+    editUserInfo(currentUserId, tagList, "tags")
+      .then((res) => {
+        if (res.status) {
+          alert(res.message);
+          setUpdateData();
+        }
+      })
+      .catch((e) => alert(e.message));
   }
-  const tagList = makeDataField(user.tags, `기술스택`, false);
-  const postList = makeDataField(user.posts, `현재 참여한 스터디`, true);
-  form.insertBefore(tagList, updateBtn);
-  form.insertBefore(postList, updateBtn);
-  // 업데이트 페이지로 버튼 이동 이벤트
-  form.addEventListener("submit", () => {
-    window.location = `/user/${user.id}/edit`;
-  });
 }
 
-// 수정버튼 이벤트 리스너
-// nameForm.addEventListener("submit", () => {
-//   const name = document.getElementById("name");
-//   fetch(`${currentUserId}/edit`, {
-//     method: "PUT",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       nickname: `${name.value}`,
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       name.placeholder = data;
-//       alert(`변경에 성공했습니다.`);
-//     });
-// });
-// tagForm.addEventListener("submit", () => {
-//   const tag = document.querySelectorAll("input[type='checkbox']");
-//   const checkedTag = document.querySelectorAll("input[type='checkbox']:checked");
-//   const taglabel = document.querySelectorAll("input[type='checkbox']:checked + label");
-//   let tagList = [];
-//   for (let i = 0; i < taglabel.length; i++) {
-//     tagList.push(`${taglabel[i].innerText}`);
-//   }
-//   fetch(`${currentUserId}/edit`, {
-//     method: "PUT",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       tags: tagList,
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       tag.alert(`변경에 성공했습니다.`);
-//     });
-// });
+// 비밀번호 확인 함수
+function confirmPassword() {
+  const currPw = document.getElementById("currPw").value;
+  const changePw = document.getElementById("changePw").value;
+  const checkPw = document.getElementById("checkPw").value;
+  if (!currPw || !changePw || !checkPw)
+    return alert("현재 비밀번호, 변경 비밀번호, 비밀번호 확인을 모두 입력해주세요!");
 
-// nameField.addEventListener("submit", btnSubmit);
-// function btnSubmit() {
-//   // 정상적으로 업데이트
-//   alert(this);
-//   console.log(this);
-//   // if (ele.value === "") {
-//   //   alert("변경하려면 값을 입력하세요");
-//   //   return;
-//   // }
-//   // alert(`변경에 성공했습니다`);
-//   // 아니라면
-//   // alert(` 변경에 실패했습니다`);
-// }
+  const userPassword = `12345`; // TODO: user password 값 가져오기
+  if (currPw !== userPassword) return alert("현재 비밀번호가 틀렸습니다.");
+  if (changePw !== checkPw) return alert("변경할 비밀번호와 비밀번호 확인이 다릅니다");
+  if (validationPw(checkPw)) {
+    // TODO : changePassword 함수 동작 확인
+    changePassword({ currentPassword: `${userPassword}`, password: `${checkPw}` })
+      .then((res) => {
+        if (res.status) {
+          alert(res.message);
+          document.location = "/signin";
+        }
+      })
+      .catch((e) => alert(e.message));
+    console.log("qusrud");
+  } else return alert("8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.");
+}
 
-//TODO : 유효성 검사
-// function confirmPw() {
-//   const nowPw = document.getElementById("nowPw").value;
-//   const chPw = document.getElementById("chPw").value;
-//   const ckPw = document.getElementById("ckPw").value;
-//   const password = `12345`; // user password 값 가져와야 함
-//   const pwField = document.getElementById("password");
-//   pwField.setAttribute("target", "iframe");
-//   if (nowPw !== password) alert("현재 비밀번호를 잘못입력했습니다");
-//   else if (chPw !== ckPw) alert("비밀번호 확인이 틀렸습니다");
-//   else {
-//     if (checkPw(chPw)) {
-//       alert(`비밀번호를 성공적으로 변경했습니다. 변경한 비밀번호로 다시 로그인해 주세요`);
-//       const pwField = document.getElementById("password");
-//       pwField.removeAttribute("target");
-//       pwField.setAttribute("action", "../../signin");
-//     } else alert("8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.");
-//   }
-// }
-// function checkPw(chPw) {
-//   const pwPattern = /[a-zA-Z0-9~!@#$%^&*()_+|<>?:{}]{8,16}/;
-//   if (chPw === "") return false;
-//   else if (!pwPattern.test(chPw)) return false;
-
-//   return true;
-// }
-
-// function checkNick() {
-//   const nickPattern = /[a-zA-Z0-9_-]{5,20}/;
-
-//   if (nickname.value === "") {
-//     blockTagExtension(error[1], "필수 정보입니다.");
-//   } else if (!nickPattern.test(nickname.value)) {
-//     blockTagExtension(error[1], "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.");
-//   } else {
-//     error[1].innerHTML = "사용 가능한 닉네임입니다.";
-//     error[1].style.color = "#7979d3";
-//   }
-// }
-
-// function checkPw() {
-//   const pwPattern = /[a-zA-Z0-9~!@#$%^&*()_+|<>?:{}]{8,16}/;
-//   if (pswd.value === "") {
-//     blockTagExtension(error[2], "필수 정보입니다.");
-//   } else if (!pwPattern.test(pswd.value)) {
-//     blockTagExtension(error[2], "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.");
-//   } else {
-//     error[2].style.display = "none";
-//   }
-// }
-
-// function comparePw() {
-//   if (pswdCf.value === pswd.value && pswdCf.value != "") {
-//     error[3].style.display = "none";
-//   } else if (pswdCf.value !== pswd.value) {
-//     blockTagExtension(error[3], "비밀번호가 일치하지 않습니다.");
-//   }
-
-//   if (pswdCf.value === "") {
-//     blockTagExtension(error[3], "필수 정보입니다.");
-//   }
-// }
+// 비밀번호 유효성 검사 함수
+function validationPw(checkPw) {
+  const pwPattern = /[a-zA-Z0-9~!@#$%^&*()_+|<>?:{}]{8,16}/;
+  if (checkPw === "") return false;
+  else if (!pwPattern.test(checkPw)) return false;
+  return true;
+}
