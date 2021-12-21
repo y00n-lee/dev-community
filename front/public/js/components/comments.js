b77987b4a12cafb036031276746b7dda057058da
Jang Byung Yeon <kisagge@naver.com>
2021-12-21 17:13:00 +0900

    feat comments 수정

--- front/public/js/components/comments.js
+++ front/public/js/components/comments.js
@@ -1,4 +1,6 @@
 import { createEleId, createEleClass, addTextNode, removeChildsAll } from "./utils.js";
+import { addComment } from "../api/dummy/index.js";
+//import { addComment } from "../api/posts/addComment.js";
 
 // Comments Header
 function cmtHeader(comments) {
@@ -23,19 +25,22 @@ function commentsLists(comments) {
 
     // Comment
     const comment = createEleClass("p", "cmtComment");
-    addTextNode(comment, `> ${comments[i].cmt}`);
+    addTextNode(comment, `> ${comments[i].content}`);
     comment.style.color = "#7979d3";
     cmtList.appendChild(comment);
     cmtLists.appendChild(cmtList);
   }
 }
 
-function addComment(comments, user) {
+function appendComment(comments, user) {
   event.preventDefault();
   if (document.getElementById("cmtComment").value.length > 0) {
     const comment = {
-      cmt: `${document.getElementById("cmtComment").value}`,
-      author: user.nickname,
+      _id: user.data.user._id,
+      author: user.data.user.nickname,
+      content: `${document.getElementById("cmtComment").value}`,
+      createAt: new Date(),
+      updateAt: new Date(),
     };
     comments.push(comment);
     cmtHeader(comments);
@@ -44,13 +49,13 @@ function addComment(comments, user) {
   }
 }
 // Comments Make
-function cmtMainTail(comments, user) {
+function cmtMainTail(post, user) {
   const cmtFormTableRow = document.getElementById("cmtFormTableRow");
   removeChildsAll(cmtFormTableRow);
   // Comment Author
   const cmtAuthorTd = document.createElement("td");
   const cmtAuthor = createEleClass("div", "cmtTableAuthor");
-  addTextNode(cmtAuthor, `${user.nickname}`);
+  addTextNode(cmtAuthor, `${user.data.user.nickname}`);
   cmtAuthorTd.appendChild(cmtAuthor);
   cmtFormTableRow.appendChild(cmtAuthorTd);
 
@@ -66,17 +71,23 @@ function cmtMainTail(comments, user) {
   const cmtBtn = createEleId("button", "cmtBtn");
   addTextNode(cmtBtn, "등록");
   cmtForm.addEventListener("submit", function () {
-    addComment(comments, user);
+    appendComment(post.comments, user);
+    addComment({ postId: post._id, content: `${document.getElementById("cmtComment").value}` })
+      .then((res) => {
+        if (!res.status) alert(res.message);
+        //else window.location = "/posts";
+      })
+      .catch((e) => alert(e.message));
   });
   cmtBtnTd.appendChild(cmtBtn);
   cmtFormTableRow.appendChild(cmtBtnTd);
 }
 
-export function makeComments(comments, user) {
+export function makeComments(post, user) {
   // 게시글 헤더
-  cmtHeader(comments);
+  cmtHeader(post.comments);
   // 게시글의 댓글 출력
-  commentsLists(comments);
+  commentsLists(post.comments);
   // 게시글의 댓글 입력 부분
-  cmtMainTail(comments, user);
+  cmtMainTail(post, user);
 }
