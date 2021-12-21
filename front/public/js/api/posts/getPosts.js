ca334af9571ce474f67123ad577c750ac78e1648
gyeongpyo <ticket5028@gmail.com>
2021-12-21 09:36:56 +0900

    add post-api function #27

---
+++ front/public/js/api/posts/getPosts.js
@@ -0,0 +1,16 @@
+export const getPosts = async ({ page, perPage }) => {
+	try {
+		const result = await fetch(`http://localhost:9999/posts?page=${page}&perPage=${perPage}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+		const res = result.json();
+
+		return res;
+	} catch (e) {
+		return { status: false, message: "서버 에러입니다." };
+	}
+};
