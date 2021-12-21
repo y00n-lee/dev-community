874fa79dff4fc06de7955533ca4afaf6dc901be1
y00n-lee <0613myday@gmail.com>
2021-12-21 15:38:59 +0900

    페이지 확인을 위한 더미데이터 수정

--- front/public/js/api/dummy/index.js
+++ front/public/js/api/dummy/index.js
@@ -22,7 +22,7 @@ export const onSignIn = (data) => ({
   },
 });
 
-export const getUserInfo = (id) => ({
+export const getUserInfo = async (id) => ({
   status: true,
   data: {
     user: {
@@ -31,8 +31,11 @@ export const getUserInfo = (id) => ({
       nickname: "지석호",
       birth: "2021-10-06T15:00:00.000Z",
       gender: "male",
-      tags: [],
-      posts: [],
+      tags: ["js", "react", "vue", "mongoDB"],
+      posts: [
+        { title: "react프로젝트 하실 분 구합니다!", id: 10 },
+        { title: "CS 공부할 사람 모여라~", id: 2 },
+      ],
       emailVerified: false,
       keyForVerify: "e0d393a393ffdabc6767fFQtFD/+AiiB6crhqDPm",
       passwordReset: false,
@@ -40,6 +43,7 @@ export const getUserInfo = (id) => ({
       updatedAt: "2021-12-20T04:46:44.333Z",
       __v: 0,
     },
+    same: true,
   },
 });
 
