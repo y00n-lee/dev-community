918d54de2e5e16f6688f058a1251035dd12bc6b3
Jang Byung Yeon <kisagge@naver.com>
2021-12-21 17:13:12 +0900

    feat 글상세 async 처리 일부

--- front/public/js/gatherboardDetail.js
+++ front/public/js/gatherboardDetail.js
@@ -1,76 +1,97 @@
 import { makeHeader } from "./components/header.js";
 import { makeFooter } from "./components/footer.js";
 import { makeComments } from "./components/comments.js";
-import { createEleClass, addTextNode } from "./components/utils.js";
-// User Function
+import { addTextNode } from "./components/utils.js";
 
-// Make Button Function
-function makeLinkButton(link, content) {
-  const div = createEleClass("div", "btn_area");
-  const btn = createEleClass("button", "btn");
+//import { getPost } from "./api/posts/getPost.js";
+import { getPost, getUserInfo, editPost, deletePost, addMember } from "./api/dummy/index.js";
+//import { getUserInfo } from "./api/user/getUserInfo.js";
+//import { editPost } from "./api/posts/editPost.js";
+//import { deletePost } from "./api/posts/deletePost.js";
 
-  btn.setAttribute("onclick", `location.href='${link}'`);
-  btn.appendChild(document.createTextNode(`${content}`));
-  div.appendChild(btn);
+const user = getUserInfo(1);
 
-  return div;
-}
+const pathname = window.location.pathname.split("/");
+const currentPostId = pathname[pathname.length - 1];
 
-// dummy data
-const gatherData = {
-  emailId: "kisagge@naver.com",
-  title: "프론트엔드 프로젝트 하실 분 구합니다!!",
-  content: "React나 Spring 다룰 줄 아시는 분 구합니다. 가능하신 분은 연락주세요!!!",
-  techStack: ["React", "Spring"],
-  looks: 0,
-  comments: [
-    {
-      cmt: "연락주세요",
-      author: "Yuna Kim",
-    },
-    {
-      cmt: "가능합니다!",
-      author: "James",
-    },
-  ],
-};
-
-const user = {
-  emailId: "kisagge@naver.com",
-  nickname: "GgemKko",
-};
+const postData = getPost(currentPostId);
+// .then((res) => {
+//   if (!res.status) alert(res.message);
+// })
+// .catch((e) => alert(e.message));
 
 const container = document.querySelector(".container");
 // Header
 const header = makeHeader();
 container.prepend(header);
-// Main
-const main = document.getElementById("main");
 // Gather Title
-addTextNode(document.getElementById("gatherTitle"), `${gatherData.title}`);
+addTextNode(document.getElementById("gatherTitle"), `${postData.data.post.title}`);
 // Gather Content
 document
   .getElementById("gatherContent")
-  .appendChild(document.createTextNode(`${gatherData.content}`));
+  .appendChild(document.createTextNode(`${postData.data.post.content}`));
 
 // Gather TechStack
 const gatherTechStackSpan = document.getElementById("gatherTechStack");
-for (let i = 0; i < gatherData.techStack.length; i++) {
+for (let i = 0; i < postData.data.post.tags.length; i++) {
   const techStackImg = document.createElement("p");
-  techStackImg.appendChild(document.createTextNode(`${gatherData.techStack[i]}`));
+  techStackImg.appendChild(document.createTextNode(`${postData.data.post.tags[i].content}`));
   gatherTechStackSpan.appendChild(techStackImg);
 }
 
 //Button
-if (user.emailId == gatherData.emailId) {
+// "sukho1007@naver.com"
+// 글 작성자와 유저가 같은 경우와 다른 경우
+if (user.data.user.email == postData.data.post.author.email) {
   document.getElementById("update").style.display = "block";
+  document.getElementById("delete").style.display = "block";
   document.getElementById("participate").style.display = "none";
 } else {
   document.getElementById("update").style.display = "none";
+  document.getElementById("delete").style.display = "none";
   document.getElementById("participate").style.display = "block";
 }
 
-makeComments(gatherData.comments, user);
+// document.getElementById("update").addEventListener("click", function () {
+//   editPost({
+//     postId: postData.data.post._id,
+//     title: postData.data.post.title,
+//     content: postData.data.post.content,
+//     tagList: postData.data.post.tags,
+//   })
+//     .then((res) => {
+//       alert(res.message);
+//       window.location = `/edit/post/${currentPostId}`;
+//     })
+//     .catch((e) => alert(e.message));
+// });
+
+document.getElementById("delete").addEventListener("click", function () {
+  deletePost(postData.data.post._id)
+    .then((res) => {
+      alert(res.message);
+      window.location = "/posts";
+    })
+    .catch((e) => alert(e.message));
+});
+
+document.getElementById("participate").addEventListener("click", function () {
+  addMember(postData.data.post._id)
+    .then((res) => {
+      if (!res.status) alert(res.message);
+      else {
+        alert(res.message);
+        window.location = "/posts";
+      }
+    })
+    .catch((e) => alert(e.message));
+});
+
+document.getElementById("gotoList").addEventListener("click", function () {
+  window.location.href = "/posts";
+});
+
+makeComments(postData.data.post, user);
 
 //footer
 const footer = makeFooter();
