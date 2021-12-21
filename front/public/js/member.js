634dfdc70a26d9fb7cceb9a58dff368db0ea4b13
y00n-lee <0613myday@gmail.com>
2021-12-21 19:17:12 +0900

    feat. member 페이지 api 연결

--- front/public/js/member.js
+++ front/public/js/member.js
@@ -1,6 +1,7 @@
 import { makeHeader } from "./components/header.js";
 import { makeFooter } from "./components/footer.js";
-import { createEleClass, addTextNode } from "./components/utils.js";
+import { createEleClass, addTextNode, addChild } from "./components/utils.js";
+import { getPost } from "./api/dummy/index.js";
 
 //DOM elements
 const body = document.querySelector("body");
@@ -13,31 +14,29 @@ const section = document.querySelector("#main section");
 body.insertBefore(header, main);
 body.insertBefore(footer, document.querySelector("script"));
 
-// set page title
-addTextNode(document.querySelector(".group-title"), "이 프로젝트 그룹 이름");
+const pathname = window.location.pathname.split("/");
+const currentPostId = pathname[pathname.length - 1];
 
-// // 각각의 멤버 카드 article로 생성
-//TODO : 그룹 멤버 수 데이터 가져오기
-const memberCount = 5;
-//TODO : 각 멤버 데이터 가져오기
-const stacks = ["자바스크립트", "노드js", "react", "vue", "mongodb"];
+// get user data
+window.onload = (function () {
+  const post = getPost(currentPostId).data.post;
+  const members = post.members;
+  addTextNode(document.querySelector(".group-title"), `${post.title}`);
+  for (let i = 0; i < members.length; i++) {
+    makeMemberCard(members[i]);
+  }
+})();
 
-for (let i = 0; i < memberCount; i++) {
+// 멤버 카드 생성 함수
+function makeMemberCard(user) {
   const article = createEleClass("article", "flex-item");
-  const memberImg = createEleClass("img", "image"); // 회원 프로필 사진
   const memberName = createEleClass("p", "member-name"); // 회원 이름
-  const memberIntro = createEleClass("p", "introduce"); // 회원 소개말
+  const memberEmail = createEleClass("p", "introduce"); // 회원 소개말
   const memberSkill = createEleClass("p", "member-skill"); // 회원 기술 스택
 
-  // 회원 데이터 불러오기
-  memberImg.setAttribute("src", "../img/about/man1.png");
-  addTextNode(memberName, "김병철");
-  addTextNode(memberIntro, `안녕하세요, 잘 부탁드립니다~~!안녕하세요, 잘 부탁드립니다~~!`);
-  addTextNode(memberSkill, stacks[i]);
-  article.appendChild(memberImg);
-  article.appendChild(memberName);
-  article.appendChild(memberIntro);
-  article.appendChild(memberSkill);
+  addChild(article, memberName, user.nickname);
+  addChild(article, memberEmail, user.email);
+  addChild(article, memberSkill, user.tags);
 
   section.appendChild(article);
 }
