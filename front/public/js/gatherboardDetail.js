3d63d83f23118c0f9b8285df65a9194d2437bcf0
Jang Byung Yeon <kisagge@naver.com>
2021-12-22 16:08:36 +0900

    feat. deleteMember 추가 / api 수정

--- front/public/js/gatherboardDetail.js
+++ front/public/js/gatherboardDetail.js
@@ -4,10 +4,11 @@ import { makeComments } from "./components/comments.js";
 import { addTextNode } from "./components/utils.js";
 
 //import { getPost } from "./api/posts/getPost.js";
-import { getPost, deletePost, addMember, checkSignin } from "./api/dummy/index.js";
+import { getPost, deletePost, addMember, checkSignin, deleteMember } from "./api/dummy/index.js";
 //import { getUserInfo } from "./api/user/getUserInfo.js";
 //import { editPost } from "./api/posts/editPost.js";
 //import { deletePost } from "./api/posts/deletePost.js";
+//import { deleteMember } from "./api/posts/deleteMember.js";
 
 // 사용자 함수
 // Post Construction
@@ -26,7 +27,7 @@ function postConstruction(post) {
 }
 
 function setDisplayButtons(user, post) {
-  if (user.id === post._id) {
+  if (user.id === post.author._id) {
     document.getElementById("delete").style.display = "block";
     document.getElementById("participate").style.display = "none";
   } else {
@@ -39,7 +40,7 @@ function setDeleteButton(postId) {
   document.getElementById("delete").addEventListener("click", function () {
     deletePost(postId)
       .then((res) => {
-        alert(res.message);
+        if (!res.status) return alert(res.message);
         window.location = "/posts";
       })
       .catch((e) => alert(e.message));
@@ -47,23 +48,38 @@ function setDeleteButton(postId) {
 }
 
 // Setting Participation button
-function setParticipateButton(postId, signInStatus) {
-  if (!signInStatus) {
-    alert("로그인 후 이용 가능합니다.");
-    window.location = "/signin";
+function setParticipateButton(post, user) {
+  const memberIds = [];
+  post.members.forEach((el) => {
+    memberIds.push(el._id);
+  });
+  if (memberIds.includes(user.data.id)) {
+    document.getElementById("participate").innerText = "참가해제";
   } else {
-    document.getElementById("participate").addEventListener("click", function () {
-      addMember(postId)
-        .then((res) => {
-          if (!res.status) alert(res.message);
-          else {
-            alert(res.message);
-            window.location = "/posts";
-          }
-        })
-        .catch((e) => alert(e.message));
-    });
+    document.getElementById("participate").innerText = "참가하기";
   }
+
+  document.getElementById("participate").addEventListener("click", function () {
+    if (!user.status) {
+      alert("로그인 후 이용 가능합니다.");
+    } else {
+      if (memberIds.includes(user.data.id)) {
+        deleteMember(post._id)
+          .then((res) => {
+            alert(res.message);
+            if (res.status) window.location.reload();
+          })
+          .catch((e) => alert(e.message));
+      } else {
+        addMember(post._id)
+          .then((res) => {
+            alert(res.message);
+            if (res.status) window.location.reload();
+          })
+          .catch((e) => alert(e.message));
+      }
+    }
+  });
 }
 
 const pathname = window.location.pathname.split("/");
@@ -89,11 +105,11 @@ getPost(currentPostId)
     if (!res.status) alert(res.message);
     else {
       postConstruction(res.data.post);
+      setDeleteButton(res.data.post._id);
       checkSignin()
         .then((res1) => {
           setDisplayButtons(res1.data, res.data.post);
-          setDeleteButton(res.data.post._id);
-          setParticipateButton(res.data.post._id, res1.status);
+          setParticipateButton(res.data.post, res1);
           // Comments
           makeComments(res.data.post, res1.data);
         })
