649dcb4a6945c727d9972396d3c490518cbcff9a
y00n-lee <0613myday@gmail.com>
2021-12-22 19:18:50 +0900

    fix. getPost함수에 then, catch 사용

--- front/public/js/pages/member.js
+++ front/public/js/pages/member.js
@@ -20,25 +20,28 @@ const currentPostId = pathname[pathname.length - 1];
 
 // get user data
 window.onload = (function () {
-  const post = getPost(currentPostId).data.post;
-  const members = post.members;
-  addTextNode(document.querySelector(".group-title"), `${post.title}`);
-  for (let i = 0; i < members.length; i++) {
-    makeMemberCard(members[i]);
-  }
+  getPost(currentPostId)
+    .then((res) => {
+      const post = res.data.post;
+      const members = post.members;
+      addTextNode(document.querySelector(".page-title"), `${post.title}`);
+      for (let i = 0; i < members.length; i++) {
+        const { nickname, tags, email } = members[i];
+        makeMemberCard(nickname, tags, email);
+      }
+    })
+    .catch((e) => alert(e.message));
 })();
 
 // 멤버 카드 생성 함수
-function makeMemberCard(user) {
+function makeMemberCard(nickname, tags, email) {
   const article = createEleClass("article", "flex-item");
   const memberName = createEleClass("p", "member-name"); // 회원 이름
   const memberEmail = createEleClass("p", "email"); // 회원 이메일
 
-  addChild(article, memberName, user.nickname);
-  for (let i = 0; i < user.tags.length; i++) {
-    article.appendChild(makeSkillTag(user.tags[i]));
-  }
-  addChild(article, memberEmail, user.email);
+  addChild(article, memberName, nickname);
+  for (let i = 0; i < tags.length; i++) article.appendChild(makeSkillTag(tags[i], true, false));
 
+  addChild(article, memberEmail, email);
   section.appendChild(article);
 }
