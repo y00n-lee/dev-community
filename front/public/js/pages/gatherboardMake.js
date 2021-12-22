3210a0c25c1e1758ac18ffb2c44c1cf9713f828a
Jang Byung Yeon <kisagge@naver.com>
2021-12-22 22:10:33 +0900

    feat. 글작성 기술 태그 작성부분 수정

---
+++ front/public/js/pages/gatherboardMake.js
@@ -0,0 +1,73 @@
+import { makeHeader } from "../components/header.js";
+import { makeFooter } from "../components/footer.js";
+//import { createPost } from "./api/posts/createPost.js";
+import { createPost } from "../api/dummy/index.js";
+import { makeSkillTag, selectTag } from "../components/tag.js";
+import { removeChildsAll } from "../components/utils.js";
+
+// dummy data
+const techStackData = [];
+
+// TechStack Checkbox
+function tagBox(techStack, inputId) {
+  const div = document.getElementById("tagForm");
+  techStack.forEach((el) => {
+    div.appendChild(makeSkillTag(el, true, false));
+  });
+  div.innerHTML += `<span class="box">
+  <input id="${inputId}" type="text" name="${inputId}" class="int" maxlength="40"/>
+</span>`;
+}
+
+//Body
+const container = document.querySelector(".container");
+
+// Header
+const header = makeHeader();
+container.prepend(header);
+
+// Main
+tagBox(techStackData, "techTag");
+
+document.getElementById("tagForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  let techTagValue = document.getElementById("techTag").value;
+  techStackData.push(techTagValue);
+  removeChildsAll(document.getElementById("tagForm"));
+  tagBox(techStackData, "techTag");
+  document.getElementById("techTag").focus();
+});
+
+//button
+document.getElementById("make").addEventListener("click", function () {
+  const _title = document.getElementById("title").value;
+  const _content = document.getElementById("content").value;
+  const tagsList = selectTag();
+
+  if (!_title) {
+    alert("제목을 입력하세요!");
+  } else if (!_content) {
+    alert("내용을 입력하세요!");
+  } else if (tagsList.length < 1) {
+    alert("모집하고자 하는 기술 태그를 체크하세요!");
+  } else {
+    createPost({
+      title: _title,
+      content: _content,
+      tagList: tagsList,
+    })
+      .then((res) => {
+        alert(res.message);
+        if (res.status) window.location = "/posts";
+      })
+      .catch((e) => alert(e.message));
+  }
+});
+
+document.getElementById("cancel").addEventListener("click", function () {
+  window.location = "/posts";
+});
+
+// Footer
+const footer = makeFooter();
+container.appendChild(footer);
