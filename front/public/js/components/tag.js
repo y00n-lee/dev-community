f2c3e703bfc16dd5ee856311043618a27e2de7c0
y00n-lee <0613myday@gmail.com>
2021-12-22 14:37:31 +0900

    feat. tag toggle 기능 추가

--- front/public/js/components/tag.js
+++ front/public/js/components/tag.js
@@ -1,6 +1,23 @@
 // 태그를 만드는 함수
-export function makeSkillTag(data) {
-  const label = document.createElement("label");
-  label.innerHTML = `<input type="checkbox" checked disabled style="display:none"/>${data}`;
-  return label;
+export function makeSkillTag(data, toggle) {
+  const span = document.createElement("span");
+  if (toggle)
+    span.innerHTML = `<input class="tag" type="checkbox" checked style="display:none"><span class="tagName select">${data}</span>`;
+  else
+    span.innerHTML = `<input class="tag" type="checkbox" checked disabled style="display:none"><span class="tagName select">${data}</span>`;
+  return span;
+}
+
+// 태그 토글 함수
+export function toggleTag() {
+  const input = this.querySelector(".tag");
+  const span = this.querySelector(".tagName");
+  console.log(input.checked);
+  input.checked = !input.checked;
+  if (input.checked) {
+    span.classList.add("select");
+  } else {
+    span.classList.remove("select");
+  }
+  console.log(input.checked);
 }
