c40b8b8490135f6da489834df4f6c58d7e319cff
Yoonseong Kim <rladbstjd96@naver.com>
2021-12-23 01:07:48 +0900

    feat 기능수정

--- front/public/js/pages/user/issuedPassword.js
+++ front/public/js/pages/user/issuedPassword.js
@@ -14,9 +14,6 @@ const searchSection = document.querySelector(".searchBox");
 container.appendChild(searchSection);
 
 // 유효성 검사
-// 아이디 입력 여부
-// - 아이디가 있다면 임시 비밀번호가 발송되었다는 메시지와 로그인창 이동
-// - 아이디가 없다면 가입된 정보가 없습니다.
 const searchForm = document.querySelector("#searchForm");
 const formEmail = document.querySelector(".id");
 
--- front/public/js/pages/user/signin.js
+++ front/public/js/pages/user/signin.js
@@ -14,9 +14,6 @@ const loginSection = document.querySelector(".loginBox");
 container.appendChild(loginSection);
 
 // 유효성 검사
-// 아이디 비밀번호가 db와 일치하는지 체크 (일치하면 로그인)
-// - 아이디가 db에 있고 비밀번호가 틀리다면 -> 비밀번호를 확인해주세요
-// - 아이디가 db에 없다면 -> 가입된 정보가 없습니다.
 const loginForm = document.querySelector("#loginForm");
 const formEmail = document.querySelector(".loginId");
 const formPassword = document.querySelector(".loginPassword");
@@ -26,10 +23,10 @@ loginForm.addEventListener("submit", (e) => {
 
   if (!formEmail.value || !formPassword.value) {
     alert("아이디와 비밀번호를 입력해주세요!");
-  } else if (formPassword.value.length <= 8) {
+  } else if (formPassword.value.length < 8) {
     alert("비밀번호는 8자리 이상입니다.");
   } else {
-    onSignIn({ email: "sukho1007@naver.com", password: "1007" })
+    onSignIn({ email: formEmail.value, password: formPassword.value })
       .then((res) => {
         if (!res.status) alert(res.message);
         else window.location = "/";
