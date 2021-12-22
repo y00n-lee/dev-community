f421dbe5d99965f10b08c4eff5f114ab61d0e5af
y00n-lee <0613myday@gmail.com>
2021-12-22 15:00:21 +0900

    chore. 태그 컴포넌트 수정

--- front/public/js/components/tag.js
+++ front/public/js/components/tag.js
@@ -1,23 +1,24 @@
+import { isNull } from "./utils.js";
+
 // 태그를 만드는 함수
-export function makeSkillTag(data, toggle) {
+export function makeSkillTag(data, select, toggle) {
   const span = document.createElement("span");
-  if (toggle)
-    span.innerHTML = `<input class="tag" type="checkbox" checked style="display:none"><span class="tagName select">${data}</span>`;
-  else
-    span.innerHTML = `<input class="tag" type="checkbox" checked disabled style="display:none"><span class="tagName select">${data}</span>`;
+  select = isNull([select]) ? "" : "select";
+  toggle = isNull([toggle]) ? "" : "toggle";
+  span.innerHTML = `<input class="tag" type="checkbox" checked style="display:none"><span class="tagName ${select} ${toggle}">${data}</span>`;
   return span;
 }
 
 // 태그 토글 함수
 export function toggleTag() {
   const input = this.querySelector(".tag");
-  const span = this.querySelector(".tagName");
-  console.log(input.checked);
+  const span = this.querySelector(".toggle");
+  if (!span) return;
+
   input.checked = !input.checked;
   if (input.checked) {
     span.classList.add("select");
   } else {
     span.classList.remove("select");
   }
-  console.log(input.checked);
 }
