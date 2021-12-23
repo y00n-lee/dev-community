f3fc2edfcadf6974a70eee66ee18a6e4d873acf8
y00n-lee <0613myday@gmail.com>
2021-12-23 17:52:36 +0900

    feat. 반응형 푸터

--- front/public/js/components/footer.js
+++ front/public/js/components/footer.js
@@ -18,12 +18,9 @@ export function footerPosition() {
   const footer = document.getElementById("footer").style;
   const height = document.getElementsByClassName("container")[0].offsetHeight;
 
-  console.log(footer.position);
-  if (window.innerHeight <= height) footer.position = "relative";
-  else {
+  if (window.innerHeight <= height) {
+    footer.position = "relative";
+  } else {
     footer.position = "fixed";
-    footer.bottom = "0";
-    footer.left = "0";
-    footer.right = "0";
   }
 }
