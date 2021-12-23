0f0500016959ab7b21faab19cd8cb808b3f2cce5
Yoonseong Kim <rladbstjd96@naver.com>
2021-12-23 21:17:05 +0900

    게시글 카드 디자인 및 폴더 이동

--- front/public/js/components/post.js
+++ front/public/js/components/post.js
@@ -1,29 +1,42 @@
 import { createEleClass } from "./utils.js";
 
 export function Post(data) {
+  console.log(data);
   const postCard = createEleClass("article", "post");
   const title = createEleClass("h2", "post-title");
-  const author = createEleClass("h4", "post-author");
-  const tags = createEleClass("strong", "post-stack");
+  const author = createEleClass("h5", "post-author");
+  const tagsList = createEleClass("div", "tags-list");
   const content = createEleClass("p", "post-content");
+  const writeDate = createEleClass("span", "post-date");
   const views = createEleClass("span", "post-look");
 
   title.innerText = data.title;
   author.innerText = data.author.nickname;
 
   for (let i = 0; i < data.tags.length; i++) {
-    tags.innerText += data.tags[i].content + " ";
+    const tags = createEleClass("span", "post-stack");
+    tags.innerText = data.tags[i].content;
+    tagsList.appendChild(tags);
   }
   content.innerText = data.content;
-  views.innerText = data.views;
+  const happyNewYear = new Date(data.author.createdAt);
+  const year = happyNewYear.getFullYear();
+  const month = happyNewYear.getMonth() + 1;
+  const date = happyNewYear.getDate();
+  const formatDate = `${year}-${month >= 10 ? month : "0" + month}-${
+    date >= 10 ? date : "0" + date
+  }`;
+  writeDate.innerText = formatDate;
+  views.innerText = `조회수 ${data.views}`;
 
   if (content.innerText.length > 50) {
     content.innerText = content.innerText.substring(0, 49) + "...";
   }
   postCard.appendChild(title);
   postCard.appendChild(author);
-  postCard.appendChild(tags);
+  postCard.appendChild(tagsList);
   postCard.appendChild(content);
+  postCard.appendChild(writeDate);
   postCard.appendChild(views);
 
   postCard.addEventListener("click", () => {
